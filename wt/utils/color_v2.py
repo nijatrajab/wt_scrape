1b13e9babb3f24400a0a9474792caf2bbcf2ab4d
Nijat <nicatrecebli1996@gmail.com>
2022-02-07 06:43:17 +0400

    Fixed scrape_v2 (color)

--- wt/utils/color_v2.py
+++ wt/utils/color_v2.py
@@ -16,8 +16,18 @@ def convert_rgb_to_names(hex_color):
         rgb_values.append(hex_to_rgb(color_hex))
 
     kdt_db = KDTree(rgb_values)
+    hex_color_edited = hex_color
+
     if re.match('#', hex_color, re.I) is None:
         hex_color_edited = '#' + hex_color
-    rgb_tuple = hex_to_rgb(hex_color_edited)
-    distance, index = kdt_db.query(rgb_tuple)
-    return names[index]
+        if len(hex_color_edited) > 7:
+            hex_color_edited = hex_color_edited[:7]
+    elif len(hex_color_edited) > 7:
+        hex_color_edited = hex_color_edited[:7]
+
+    try:
+        rgb_tuple = hex_to_rgb(hex_color_edited)
+        distance, index = kdt_db.query(rgb_tuple)
+    except:
+        names = None
+    return names[index] if names is not None else names
